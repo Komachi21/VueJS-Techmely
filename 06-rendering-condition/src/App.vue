474ae88160f5e577deb582b6cd2c93cf737380b0
Nguyen Ngoc Hieu <HieuNguyen@mor.com.vn>
2024-07-31 11:25:19 +0700

    06

---
+++ 06-rendering-condition/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+const isActive = ref(false)
+const display = () => {
+  isActive.value = !isActive.value
+}
+</script>
+
+<template>
+  <div>
+    <h1 v-show="isActive">Displayed V-Show</h1>
+    <h1 v-if="isActive">Displayed V-If</h1>
+    <h1 v-else>Displayed V-Else</h1>
+    <button @click="display">Active</button>
+  </div>
+</template>
+
+<style></style>
